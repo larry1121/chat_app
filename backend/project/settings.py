0db804d264565a7c9ea3e9d9104751ec0b3e4b45
정재윤 <jaeyoonjung01@gmail.com>
2024-08-12 15:20:41 +0900

    db 설정 변경

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -144,12 +144,10 @@ WSGI_APPLICATION = 'backend.project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config()
 }
 
 # Password validation
