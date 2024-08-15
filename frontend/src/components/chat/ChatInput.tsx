537b9d3519b4a0bbc56fe895a59d461e7f3bd2ef
Jaewon Youn <78005200+larry1121@users.noreply.github.com>
2024-08-15 20:02:36 +0900

    ChatInput.tsx 업데이트

--- frontend/src/components/chat/ChatInput.tsx
+++ frontend/src/components/chat/ChatInput.tsx
@@ -1,116 +1,217 @@
-import React, { useState } from 'react';
-import TextareaAutosize from 'react-textarea-autosize';
+import React, { useEffect, useRef, useState } from 'react';
+import { Sidebar } from './components/chat/Sidebar';
+import { ChatBox } from './components/chat/ChatBox';
+import { ChatInput } from "./components/chat/ChatInput";
 import styled from 'styled-components';
-import { Message } from "../../data/Message";
-import { FaPaperPlane } from 'react-icons/fa'; // Import the paper plane icon
+import ReconnectingWebSocket from "reconnecting-websocket";
+import { Message } from "./data/Message";
+import { ChatMenu } from "./components/chat/debug/ChatMenu";
+import { DebugDrawer } from "./components/chat/debug/DebugDrawer";
+import { GuidePage } from './components/chat/GuidePage'; // GuidePage import
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-type ChatInputProps = {
-  onNewUserMessage: (chatId: string, message: Message) => void;
-  onNewChatCreated: (chatId: string) => void;
-  chatId: string | null;
-};
+export const App = () => {
+  const [currentChatId, setCurrentChatId] = useState<string | null>(null);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const webSocket = useRef<ReconnectingWebSocket | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [debugMessage, setDebugMessage] = useState<string>("");
+  const [debugMode, setDebugMode] = useState<boolean>(false);
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
 
-export const ChatInput: React.FC<ChatInputProps> = ({onNewUserMessage, onNewChatCreated, chatId}) => {
-    const [message, setMessage] = useState('');
+  useEffect(() => {
+    if (currentChatId) {
+      webSocket.current = new ReconnectingWebSocket(`wss://kupletalk-4c60f80926c4.herokuapp.com/ws/chat/${currentChatId}/`);
+      webSocket.current.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        if (data.type === "debug") {
+          const formattedToken = data.message.replace(/\n/g, '<br />');
+          setDebugMessage(prevMessage => prevMessage + formattedToken);
+        } else {
+          setLoading(false);
+          const newMessage = {sender: '쿠플봇', content: data['message']};
+          setMessages(prevMessages => [...prevMessages, newMessage]);
+        }
+      };
 
-    const handleSubmit = (event: React.FormEvent) => {
-      event.preventDefault();
-
-      if (message.trim() === '') return;
-
-      if (chatId) {
-        // If there is a chatId, just send the message.
-        const newMessage = {sender: 'USER', content: message};
-        onNewUserMessage(chatId, newMessage)
-      } else {
-        // If there is no chatId, create a new chat.
-        createChat()
-      }
-      setMessage(''); // Clear the input message
+      webSocket.current.onclose = () => {
+        console.error('Chat socket closed unexpectedly');
+      };
+      fetchMessages(currentChatId);
     }
+    return () => {
+      webSocket.current?.close();
+    };
+  }, [currentChatId]);
+
+  const onChatSelected = (chatId: string | null) => {
+    if (currentChatId === chatId) return;
+    if (chatId == null) {
+      setMessages([]);
+    }
+    setCurrentChatId(chatId);
+  };
+
+  const onNewUserMessage = (chatId: string, message: Message) => {
+    webSocket.current?.send(
+      JSON.stringify({
+        message: message.content,
+        chat_id: chatId,
+      })
+    );
+    setMessages(prevMessages => [...prevMessages, message]);
+    setLoading(true);
+  };
+
+  const onNewChatCreated = (chatId: string) => {
+    onChatSelected(chatId);
+  };
+
+  const fetchMessages = (currentChatId: string | null) => {
+    fetch(`${process.env.REACT_APP_API_BASE_URL}/chats/${currentChatId}/messages/`)
+      .then(response => response.json())
+      .then(data => {
+        setMessages(data);
+        console.log(data)
+      });
+  };
+
+  const onExampleQuestionClick = (message: string) => {
+    if (message.trim() === '') return;
 
     const createChat = () => {
       fetch(`${process.env.REACT_APP_API_BASE_URL}/chats/`, {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({name: 'New Chat'}) // Adjust this as necessary.
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: 'New Chat' }) // Adjust this as necessary.
       })
         .then((response) => response.json())
         .then((newChat) => {
-          // Update listeners that a new chat was created.
-          onNewChatCreated(newChat.id)
+          onNewChatCreated(newChat.id);
 
-          // Send the message after a timeout to ensure that the Chat has been created
           setTimeout(function () {
-            // This block of code will be executed after 0.5 seconds
-            onNewUserMessage(newChat.id, {sender: 'USER', content: message})
+            onNewUserMessage(newChat.id, { sender: 'USER', content: message });
           }, 500);
         });
     };
 
-    return (
-      <Form onSubmit={handleSubmit}>
-        <StyledTextareaAutosize
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="고려대 공간에 대해 궁금한 점을 질문해주세요!"
-          maxRows={10}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' && !e.shiftKey) {
-              e.preventDefault();
-              handleSubmit(e);
-            }
-          }}
-        />
-        <Button type="submit">
-          <FaPaperPlane />
-        </Button>
-      </Form>
-    );
-  }
-;
+    createChat();
+  };
 
-const Form = styled.form`
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+
+  return (
+    <AppContainer isSidebarOpen={sidebarOpen}>
+      <SidebarContainer isOpen={sidebarOpen}>
+        <Sidebar onChatSelected={onChatSelected} selectedChatId={currentChatId} isOpen={sidebarOpen} onClose={closeSidebar} />
+      </SidebarContainer>
+      {sidebarOpen && window.innerWidth <= 768 && <Overlay onClick={closeSidebar} />}
+      <ChatContainer debugMode={debugMode} isSidebarOpen={sidebarOpen}>
+        <StyledMenuButton onClick={toggleSidebar} title={sidebarOpen ? "사이드바 닫기" : "사이드바 열기"}>
+          <FontAwesomeIcon icon={faBars} />
+        </StyledMenuButton>
+        <ChatMenu debugMode={debugMode} setDebugMode={setDebugMode} />
+        {currentChatId ? (
+          <>
+            <ChatBox messages={messages} isLoading={loading} />
+            <ChatInputContainer>
+              <ChatInput onNewUserMessage={onNewUserMessage} onNewChatCreated={onNewChatCreated} chatId={currentChatId} />
+            </ChatInputContainer>
+          </>
+        ) : (
+          <>
+            <GuidePage onExampleQuestionClick={onExampleQuestionClick} />
+          </>
+        )}
+      </ChatContainer>
+      {debugMode && <DebugDrawer message={debugMessage} debugMode={debugMode} />}
+    </AppContainer>
+  );
+};
+
+const AppContainer = styled.div<{ isSidebarOpen: boolean }>`
   display: flex;
-  align-items: flex-start;
-  padding: 10px;
-  border-top: 1px solid #eee;
-`;
+  height: 100vh;
+  overflow: hidden; // Prevent horizontal scroll
 
-const StyledTextareaAutosize = styled(TextareaAutosize)`
-  flex-grow: 1;
-  border: 1px solid #eee;
-  background-color: #f4f4f4;
-  border-radius: 3px;
-  padding: 10px;
-  margin-right: 10px;
-  resize: none;
-  overflow: auto;
-  font-family: inherit;
-  font-size: 16px;
-  min-height: 14px; // Initial height
-  max-height: 500px; // Max height
-  &:focus,
-  &:active {
-    border-color: #a33b39;
-    outline: none;
+  @media (max-width: 768px) {
+    flex-direction: column;
   }
 `;
 
-const Button = styled.button`
-  height: 40px;
-  padding: 10px 20px;
+const SidebarContainer = styled.div<{ isOpen: boolean }>`
+  width: 250px;
+  transition: transform 0.3s ease-in-out;
+  transform: ${({ isOpen }) => (isOpen ? 'translateX(0)' : 'translateX(-100%)')};
+
+  @media (max-width: 768px) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    z-index: 1000;
+  }
+`;
+
+const Overlay = styled.div`
+  @media (max-width: 768px) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    z-index: 500;
+  }
+`;
+
+const ChatContainer = styled.div<{ debugMode: boolean; isSidebarOpen: boolean }>`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  width: ${({ debugMode }) => (debugMode ? '70%' : '100%')};
+  margin-left: ${({ isSidebarOpen }) => (isSidebarOpen ? '0' : '-250px')};
+  transition: all 0.3s ease-in-out;
+  position: relative;
+  overflow-y: auto; // Allow scrolling when content overflows
+
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-left: 0;
+  }
+`;
+
+const ChatInputContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background-color: white;
+  padding: 10px;
+  box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const StyledMenuButton = styled.button`
+  position: fixed;
+  top: 16px;
+  left: 10px;
+  background: none;
   border: none;
-  background-color: #a33b39;
-  color: white;
+  font-size: 1.5em;
   cursor: pointer;
-  border-radius: 3px;
-  font-size: 1em;
-  align-self: flex-end;
-  &:hover {
-    background-color: #cd7270; /* Change this to the desired lighter shade */
-  }
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  z-index: 1100;
+  color: #f4f4f4; // Change icon color to light gray
+  font-size: 1.7em; // Match the size of new chat icon
+`;
+
+const ChatBox = styled.div`
+  flex: 1;
+  padding-bottom: 60px; // Ensure the chat messages are not hidden behind the ChatInput
+  overflow-y: auto;
 `;
