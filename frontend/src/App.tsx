087d1fe6221949fe2d9eaae7f9643cb833d8af5d
Jaewon Youn <78005200+larry1121@users.noreply.github.com>
2024-08-16 19:43:00 +0900

    App.tsx 업데이트

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -163,8 +163,8 @@ const Overlay = styled.div<{ isSidebarOpen: boolean }>`
   @media (max-width: 768px) {
     position: fixed;
     top: 0;
-    left: 0;
-    width: 100%;
+    left: 250px; /* 사이드바의 폭만큼 왼쪽에서 시작 */
+    width: calc(100% - 250px); /* 사이드바를 제외한 나머지 영역을 덮음 */
     height: 100%;
     background: rgba(0, 0, 0, 0.5);
     z-index: 1500;
